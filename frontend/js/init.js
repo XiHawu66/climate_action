74755688eb68993fa6ff01c8524d4e3102e65f14
fwuu0017 <fwuu0017@student.monash.edu>
2023-05-16 00:08:08 +1000

    error msg and redirect for clicking recommendation without filling the survey

--- frontend/js/init.js
+++ frontend/js/init.js
@@ -262,13 +262,67 @@ jQuery(document).ready(function($){
   });
 });
 
+// window.alert = function(msg, callback) {
+//     var div = document.createElement("div");
+//     div.innerHTML = "<style type=\"text/css\">"
+//         + ".nbaMask { position: fixed; z-index: 1000; top: 0; right: 0; left: 0; bottom: 0; background: rgba(0, 0, 0, 0.5); }                                                                                                                                                                       "
+//         + ".nbaMaskTransparent { position: fixed; z-index: 1000; top: 0; right: 0; left: 0; bottom: 0; }                                                                                                                                                                                            "
+//         + ".nbaDialog { position: fixed; z-index: 5000; width: 80%; max-width: 300px; top: 20%; left: 50%; box-shadow: rgba(0, 0, 0, 0.18) 0px 2px 4px; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%); background-color: #fff; text-align: center; border-radius: 8px; overflow: hidden; opacity: 1; color: white; }"
+//         + ".nbaDialog .nbaDialogHd { padding: .2rem .27rem .08rem .27rem; background: #626E64;}                                                                                                                                                                                                                         "
+//         + ".nbaDialog .nbaDialogHd .nbaDialogTitle { font-size: 28px; font-weight: 400; }                                                                                                                                                                                                           "
+//         + ".nbaDialog .nbaDialogBd { padding: .8rem .8rem .8rem .8rem; font-size: 18px; line-height: 1.3; font-weight: normal; word-wrap: break-word; word-break: break-all; color: #FFFFFF; background: #626E64}                                                                                                                                          "
+//         + ".nbaDialog .nbaDialogFt { position: relative; line-height: 48px; font-size: 17px; display: -webkit-box; display: -webkit-flex; display: flex; }                                                                                                                                          "
+//         + ".nbaDialog .nbaDialogFt:after { content: \" \"; position: absolute; left: 0; top: 0; right: 0; height: 1px; border-top: 1px solid #e6e6e6; color: #e6e6e6; -webkit-transform-origin: 0 0; transform-origin: 0 0; -webkit-transform: scaleY(0.5); transform: scaleY(0.5); }               "
+//         + ".nbaDialog .nbaDialogBtn { display: block; -webkit-box-flex: 1; -webkit-flex: 1; flex: 1; color: #464F48; font-weight: bold; text-decoration: none; -webkit-tap-highlight-color: transparent; position: relative; margin-bottom: 0; }                                                                       "
+//         + ".nbaDialog .nbaDialogBtn:after { content: \" \"; position: absolute; left: 0; top: 0; width: 1px; bottom: 0; border-left: 1px solid #e6e6e6; color: #e6e6e6; -webkit-transform-origin: 0 0; transform-origin: 0 0; -webkit-transform: scaleX(0.5); transform: scaleX(0.5); }             "
+//         + ".nbaDialog a { text-decoration: none; -webkit-tap-highlight-color: transparent; }"
+//         + "</style>"
+//         + "<div id=\"dialogs2\" style=\"display: none\">"
+//         + "<div class=\"nbaMask\"></div>"
+//         + "<div class=\"nbaDialog\">"
+//         // + "    <div class=\"nbaDialogHd\">"
+//         // + "        <strong class=\"nbaDialogTitle\"></strong>"
+//         // + "    </div>"
+//         + "    <div class=\"nbaDialogBd\" id=\"dialog_msg2\">Information</div>"
+//             // + "    <div class=\"nbaDialogHd\">"
+//             // + "        <strong class=\"nbaDialogTitle\"></strong>"
+//             // + "    </div>"
+//         + "    <div class=\"nbaDialogFt\">"
+//         + "        <a href=\"javascript:;\" class=\"nbaDialogBtn nbaDialogBtnPrimary\" id=\"dialog_ok2\">YES</a>"
+//         + "    </div></div></div>";
+//     document.body.appendChild(div);
+//
+//     var dialogs2 = document.getElementById("dialogs2");
+//     dialogs2.style.display = 'block';
+//
+//     var dialog_msg2 = document.getElementById("dialog_msg2");
+//     dialog_msg2.innerHTML = msg;
+//
+//     var dialog_ok2 = document.getElementById("dialog_ok2");
+//     dialog_ok2.onclick = function() {
+//         dialogs2.style.display = 'none';
+//         callback();
+//     };
+// };
+
 function setRecommendationAlert() {
     let emission = window.sessionStorage.getItem('total_emission');
     if (emission === null) {
-        alert('Please fill the survey first');
-        window.location.href = "calculator.html";
+        // window.location.href = "calculator.html";
+        Swal.fire({
+            title: 'Warning!',
+            text: 'You should fill up the survey before moving to the recommendations',
+            icon: 'warning',
+            confirmButtonText: 'GO TO SURVEY',
+            confirmButtonColor: '#BCD1BF'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = "calculator.html";
+            }
+        })
     }
     else {
         window.location.href = "report.html";
     }
+
 }
